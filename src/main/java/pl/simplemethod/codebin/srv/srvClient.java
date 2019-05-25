37d1d287d7a72c942f9be9cf5aab5fe240f70d35
SimpleMethod <michal.piotr.mlodawski@gmail.com>
2019-05-25 11:04:30 +0200

    Adding minimum api support

--- src/main/java/pl/simplemethod/codebin/srv/srvClient.java
+++ src/main/java/pl/simplemethod/codebin/srv/srvClient.java
@@ -16,6 +16,25 @@ public class srvClient {
         this.SERVER_URL = serverUrl;
     }
 
+
+    private org.json.JSONObject jsonHelper(HttpResponse<JsonNode> response) {
+        org.json.JSONObject body = new org.json.JSONObject();
+        JSONParser parser = new JSONParser();
+        Object obj = null;
+
+        try {
+            obj = parser.parse(response.getBody().toString());
+            org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject) obj;
+            body.put("message", jsonObject.get("message"));
+            body.put("status", String.valueOf(response.getStatus()));
+            return body;
+        } catch (NullPointerException | ParseException | org.json.JSONException e) {
+            body.put("message", "Something went wrong:" + e);
+            body.put("status", 510);
+            return body;
+        }
+    }
+
     /**
      * Method to generate JSON used to create docker container
      *
@@ -113,19 +132,72 @@ public class srvClient {
      * @return Json object with status
      */
     public org.json.JSONObject stopContainer(String id) {
-        JSONParser parser = new JSONParser();
         org.json.JSONObject body = new org.json.JSONObject();
-        Object obj = null;
         try {
-            HttpResponse<JsonNode> stopContainer = Unirest.post(SERVER_URL + "/v1.0/containers/" + id + "/stop")
-                    .header("accept", "application/json").header("Content-Type", "application/json").asJson();
-            obj = parser.parse(stopContainer.getBody().toString());
-            org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject) obj;
-            body.put("message", jsonObject.get("message"));
-            body.put("status", String.valueOf(stopContainer.getStatus()));
+            HttpResponse<JsonNode> stopContainer = Unirest.post(SERVER_URL + "/v1.0/containers/" + id + "/stop").header("accept", "application/json").header("Content-Type", "application/json").asJson();
+            if (stopContainer.getStatus() != 204) {
+                body.put("message", "Something went wrong ");
+                body.put("status", stopContainer.getStatus());
+                return body;
+            } else {
+                body.put("status", stopContainer.getStatus());
+                return body;
+            }
+        } catch (NullPointerException | org.json.JSONException | UnirestException e) {
+            body.put("message", "Something went wrong: " + e);
+            body.put("status", 510);
             return body;
-        } catch (NullPointerException | ParseException | org.json.JSONException | UnirestException e) {
-            body.put("message", "Something went wrong");
+        }
+    }
+
+    /**
+     * Method delete the container
+     *
+     * @param id Container ID
+     * @return Json object with status
+     */
+    public org.json.JSONObject deleteContainer(String id) {
+        org.json.JSONObject body = new org.json.JSONObject();
+        try {
+            HttpResponse<JsonNode> deleteContainer = Unirest.delete(SERVER_URL + "/v1.0/containers/" + id).header("accept", "application/json").header("Content-Type", "application/json").asJson();
+            if (deleteContainer.getStatus() != 204) {
+                body.put("message", "Something went wrong ");
+                body.put("status", deleteContainer.getStatus());
+                return body;
+            } else {
+                body.put("status", deleteContainer.getStatus());
+                return body;
+            }
+        } catch (NullPointerException | org.json.JSONException | UnirestException e) {
+            body.put("message", "Something went wrong: " + e);
+            body.put("status", 510);
+            return body;
+        }
+    }
+
+    /**
+     * Method execute command on the container
+     *
+     * @param id        Container ID
+     * @param path      Path to the bash script or package
+     * @param arguments Argument to execute
+     * @return Json object with status
+     */
+    public org.json.JSONObject execContainer(String id, String path, String arguments) {
+        org.json.JSONObject body = new org.json.JSONObject();
+        try {
+            HttpResponse<JsonNode> execContainer = Unirest.post(SERVER_URL + "/v1.0/containers/" + id + "/exec")
+                    .header("accept", "application/json").header("Content-Type", "application/json").queryString("path", path).queryString("argument", arguments).asJson();
+            if (execContainer.getStatus() != 200) {
+                body.put("message", "Something went wrong ");
+                body.put("status", execContainer.getStatus());
+                return body;
+            } else {
+                body.put("status", execContainer.getStatus());
+                return body;
+            }
+        } catch (NullPointerException | org.json.JSONException | UnirestException e) {
+            body.put("message", "Something went wrong: " + e);
             body.put("status", 510);
             return body;
         }
@@ -138,21 +210,48 @@ public class srvClient {
      * @return Json object with status
      */
     public org.json.JSONObject restartContainer(String id) {
-        JSONParser parser = new JSONParser();
         org.json.JSONObject body = new org.json.JSONObject();
-        Object obj = null;
         try {
-            HttpResponse<JsonNode> stopContainer = Unirest.post(SERVER_URL + "/v1.0/containers/" + id + "/restart")
-                    .header("accept", "application/json").header("Content-Type", "application/json").asJson();
-            obj = parser.parse(stopContainer.getBody().toString());
-            org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject) obj;
-            body.put("message", jsonObject.get("message"));
-            body.put("status", String.valueOf(stopContainer.getStatus()));
-            return body;
-        } catch (NullPointerException | ParseException | org.json.JSONException | UnirestException e) {
-            body.put("message", "Something went wrong");
+            HttpResponse<JsonNode> restartContainer = Unirest.post(SERVER_URL + "/v1.0/containers/" + id + "/restart").header("accept", "application/json").header("Content-Type", "application/json").asJson();
+            if (restartContainer.getStatus() != 204) {
+                body.put("message", "Something went wrong ");
+                body.put("status", restartContainer.getStatus());
+                return body;
+            } else {
+                body.put("status", restartContainer.getStatus());
+                return body;
+            }
+        } catch (NullPointerException | org.json.JSONException | UnirestException e) {
+            body.put("message", "Something went wrong: " + e);
             body.put("status", 510);
             return body;
         }
     }
+
+    /**
+     * The method returns container logs
+     *
+     * @param id Container ID
+     * @return Json object with status
+     */
+    public String logsContainer(String id) {
+        org.json.JSONObject body = new org.json.JSONObject();
+        try {
+            HttpResponse<String> logsContainer = Unirest.get(SERVER_URL + "/v1.0/containers/" + id + "/logs")
+                    .header("accept", "application/json").header("Content-Type", "application/json").asString();
+            if (logsContainer.getStatus() != 200) {
+                body.put("message", "Something went wrong ");
+                body.put("status", logsContainer.getStatus());
+                return body.toString();
+            } else {
+                return logsContainer.getBody();
+            }
+
+        } catch (NullPointerException | org.json.JSONException | UnirestException e) {
+            body.put("message", "Something went wrong " + e);
+            body.put("status", 510);
+            return body.toString();
+        }
+    }
+
 }
--- src/main/java/pl/simplemethod/codebin/srv/srvRestController.java
+++ src/main/java/pl/simplemethod/codebin/srv/srvRestController.java
@@ -14,7 +14,7 @@ public class srvRestController {
     @Autowired
     srvClient srvClient;
 
-    // TODO: 24.05.2019  zmienić na POST
+    // TODO: 24.05.2019  zmienić na POST, delete etc.
 
     /**
      * REST for container creation
@@ -29,16 +29,92 @@ public class srvRestController {
     public @ResponseBody
     ResponseEntity createContainer(@RequestParam("dockerimage") String dockerImage, @RequestParam("exposedports") Integer exposedPorts, @RequestParam("hostport") Integer hostPort, @RequestParam("name") String name) {
         HttpHeaders headers = new HttpHeaders();
-       org.json.JSONObject response = srvClient.createAndRunContainer(srvClient.generateCreateConfig(dockerImage, exposedPorts, hostPort), name);
-        return new ResponseEntity<>(response.toString(), headers, HttpStatus.valueOf(200));
+        org.json.JSONObject response = srvClient.createAndRunContainer(srvClient.generateCreateConfig(dockerImage, exposedPorts, hostPort), name);
+        return new ResponseEntity<>(response.toString(), headers, HttpStatus.valueOf(response.getInt("status")));
     }
 
+    /**
+     * The method restarts the container
+     *
+     * @param containerId Container ID
+     * @return Json object as response
+     */
+    @GetMapping("/srv/container/restart")
+    public @ResponseBody
+    ResponseEntity restartContainer(@RequestParam("id") String containerId) {
+        HttpHeaders headers = new HttpHeaders();
+        org.json.JSONObject restartContainer = srvClient.restartContainer(containerId);
+        return new ResponseEntity<>(restartContainer.toString(), headers, HttpStatus.valueOf(restartContainer.getInt("status")));
+    }
+
+    /**
+     * @param containerId Container ID
+     * @return Json object as response
+     */
     @GetMapping("/srv/container/stop")
     public @ResponseBody
-
     ResponseEntity stopContainer(@RequestParam("id") String containerId) {
         HttpHeaders headers = new HttpHeaders();
         org.json.JSONObject response = srvClient.stopContainer(containerId);
         return new ResponseEntity<>(response.toString(), headers, HttpStatus.valueOf(response.getInt("status")));
     }
+
+    /**
+     * Start the container with the specified ID
+     *
+     * @param containerId Container ID
+     * @return Json object as response
+     */
+    @GetMapping("/srv/container/start")
+    public @ResponseBody
+    ResponseEntity startContainer(@RequestParam("id") String containerId) {
+        HttpHeaders headers = new HttpHeaders();
+        org.json.JSONObject response = srvClient.startContainer(containerId);
+        return new ResponseEntity<>(response.toString(), headers, HttpStatus.valueOf(response.getInt("status")));
+    }
+
+    /**
+     * The method returns container logs
+     *
+     * @param containerId Container ID
+     * @return Json object as response
+     */
+    @GetMapping("/srv/container/logs")
+    public @ResponseBody
+    ResponseEntity logsContainer(@RequestParam("id") String containerId) {
+        HttpHeaders headers = new HttpHeaders();
+        String response = srvClient.logsContainer(containerId);
+        return new ResponseEntity<>(response, headers, HttpStatus.valueOf(201));
+    }
+
+    /**
+     * Method delete the container
+     *
+     * @param containerId Container ID
+     * @return Json object as response
+     */
+    @GetMapping("/srv/container/delete")
+    public @ResponseBody
+    ResponseEntity deleteContainer(@RequestParam("id") String containerId) {
+        HttpHeaders headers = new HttpHeaders();
+        org.json.JSONObject response = srvClient.deleteContainer(containerId);
+        return new ResponseEntity<>(response.toString(), headers, HttpStatus.valueOf(response.getInt("status")));
+    }
+
+    /**
+     * Method execute command on the container
+     *
+     * @param containerId Container ID
+     * @param path        Path to the bash script or package
+     * @param arguments   Argument to execute
+     * @return Json object as response
+     */
+    @GetMapping("/srv/container/exec")
+    public @ResponseBody
+    ResponseEntity execContainer(@RequestParam("id") String containerId, @RequestParam("path") String path, @RequestParam("argument") String arguments) {
+        HttpHeaders headers = new HttpHeaders();
+        org.json.JSONObject response = srvClient.execContainer(containerId, path, arguments);
+        return new ResponseEntity<>(response.toString(), headers, HttpStatus.valueOf(response.getInt("status")));
+    }
+
 }
